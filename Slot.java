219461e41a9a1debe4a6621b7a16b5be7980c357
Nicolas Swiech <nicolas.swiech.84@orange.fr>
2016-09-21 21:13:41 +0200

    Update Slot.java

--- Slot.java
+++ Slot.java
@@ -1,27 +1,43 @@
 package Othello;
 
 public class Slot {
-	boolean white;
-	boolean black;
-	
-	boolean available; //clarify and agree on meaning of this
-	//if(this.white || this.black){
-	//	this available=false;
-	//}
-	
-	//not totally sure position belongs slot
-	//perhaps its enough to do like call grid[1][4].slot.flipp() 
-	//and  grid[1][3].slot.available=false;
-	
-	Position pos = new Position();
-	
-	public void flip(){
-		if(this.white || this.black){
-			this.white =! this.white;
-			this.black =! this.black;
-		}else{
-			System.out.println("Error in slot flip function.");
-		};
-	};			
-};
+	int row;
+	int col;
+	String state;
 
+	// boolean available; // clarify and agree on meaning of this
+	// if(this.white || this.black){
+	// this available=false;
+	// }
+
+	// not totally sure position belongs slot
+	// perhaps its enough to do like call grid[1][4].slot.flipp()
+	// and grid[1][3].slot.available=false;
+
+	// Position pos = new Position();
+
+	public Slot(int x, int y, String state) {
+		this.row = x;
+		this.col = y;
+		this.state = state;
+	}
+
+	public void flip() {
+		if (this.state == "white")
+			this.state = "black";
+		else if (this.state == "black")
+			this.state = "white";
+		else
+			System.out.println("Error in slot flip function.");
+		;
+	};
+	
+	public int getRow(Slot slot) {
+		return slot.row;
+	}
+	
+	public int getCol(Slot slot) {
+		return slot.col;
+	}
+
+};
